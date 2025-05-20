aee152110fd730283001d157adbcfa56f52cbdf5
Raden Cahya Kartika <black102310@gmail.com>
2025-05-20 19:25:41 +0700

    update_template

--- kartika_water_project/urls.py
+++ kartika_water_project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from main import views 
+
 
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
+    path('about/', views.about, name='about'),
+    path('index/', views.index, name='index'),
 ]
